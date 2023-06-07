a0c06fb4f91d9ae68d06636492e42ce8b67890c6
OidneR <81741995+OidneR@users.noreply.github.com>
2023-06-07 17:18:23 +0700

    Initial Commit

---
+++ smartlock/smartlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  smartlockApp.swift
+//  smartlock
+//
+//  Created by Meichel Rendio on 07/06/23.
+//
+
+import SwiftUI
+
+@main
+struct smartlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
